c26e52d278033a3bd4b1e839c49332474ff058d4
Kasparist <kasper.kokkonen@edu.sasky.fi>
2023-09-27 21:08:59 +0300

    Added main menu with two other scenes including registration...

---
+++ Taskeri/Assets/Scripts/ButtonInputs.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ButtonInputs : MonoBehaviour
+{
+   public TMP_InputField nameField;
+   public TMP_InputField passwordField;
+
+
+}
